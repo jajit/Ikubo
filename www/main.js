fd1fe9bf675c415a48389815ddef79e9dac8c1b3
Mikel Iceta Tena <rascajones@gmail.com>
2017-11-28 22:16:04 +0100

    Añadidas funciones
    Añadida descripción de cada función
    Añadidas funciones:
        kargatuHizkuntza: falta el fragmento de AJAX que carga el contenido y el código para cambiar los títulos a los elementos de la barra
        aldatuHizkuntzaBotoia: completo
        laguntza: completo

--- www/main.js
+++ www/main.js
@@ -1,4 +1,34 @@
-aldatuHizkuntza(hizkuntza)
+//Aldagai global bat, hemen gordetzen dugu uneko hizkuntza
+ hizkuntza = null;
+
+function kargatuHizkuntza(beste_hizkuntza)
 {
-    
+   
+    if ( hizkuntza === null)
+        {
+            hizkuntza = "castellano";
+        }
+    if (beste_hizkuntza !== hizkuntza)
+        {
+            aldatuHizkuntzaBotoia();
+            //TODO: HEMEN LORTZEN DUGU EDUKIA AJAX BIDEZ
+            hizkuntza = beste_hizkuntza;
+        }
 }
+
+//Hizkuntza aldatzerakoan, hauek aukeratzeko botoiak aldatu behar dira
+
+function aldatuHizkuntzaBotoia()
+{
+    document.getElementById("euskbtn").classList.toggle("active");
+    document.getElementById("castbtn").classList.toggle("active");
+}
+
+//Laguntza botoiari sakatzerakoan, laguntza orria gure hizkuntzan kargatuko dugu. Horretarako zerbitzariari hizkuntza bidaliko diogu URL aldagai moduan 
+function laguntza()
+{
+    var zer = 'laguntza.php?hizk='+ hizkuntza
+    window.location.href = zer;
+}
+
+//
