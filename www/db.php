834a2b436c5d3e57b82574f1b1bb0d835e9d5208
Mikel Iceta Tena <rascajones@gmail.com>
2018-01-06 12:21:59 +0100

    Added db.php for db operations
    Éste código se encarga de manejar los accesos a la db. Centraliza aqui todo lo de la db

---
+++ www/db.php
@@ -0,0 +1,48 @@
+<?php
+
+if(isset($_GET['id']))
+{
+    echo getBerriOsoa($_GET['id']);
+}
+//if(isset($_GET('user') and isset($_GET('password')))){TODO: BETE HAU}
+
+    function makeQuery($query){
+        $conection = mysqli_connect("localhost", "root", "", "ikubo_web");
+        if (!$conection) return -1;
+        $resultQuery = $conection->query($query);
+        mysqli_close($conection);
+        return $resultQuery;
+    }
+    function loadBerriak(){
+        $done = false;
+        $code = "";
+        $query = makeQuery("SELECT id_noticia, autor, titulo, version_corta FROM noticias");
+        while(!$done){
+            $result = $query->fetch_assoc();
+            if ($result == ""){
+                $done = true;
+            }
+            else{
+                // TODO: Hay que cambiar el onclick por una funcion que cargue la noticia en vez del login.
+                $code .="<div class='berriak-small'>";    
+                $code .="<a href='#' onclick='irekiBerria(". $result["id_noticia"] ."); return false;'>";
+                $code .="<h2 style='margin-top: 0; margin-bottom: 0; font-weight: bold;'>" . $result["titulo"] . "</h2>";
+                $code .= "</a>";
+                $code .="<h5 style='margin-top: 0; margin-bottom: 0; font-weight: bold;'>By: " . $result["autor"] . "</h5>";
+                $code .="<p style='margin-top: 0; margin-bottom: 0;'>" . $result["version_corta"] ."</p>";
+                $code .= "</div>";
+            }
+        }
+        echo $code;        
+    }
+    function getBerriOsoa($id_berria){
+        $query = makeQuery("SELECT * FROM noticias WHERE id_noticia=". $id_berria ."" );
+        $result = $query->fetch_assoc();
+        $code = "";
+        $code .="<h2>". $result["titulo"] ."</h2>";
+        $code .="<h5>By: ". $result["autor"] . "</h5>";
+        $code .="<p>". $result["version_completa"] ."</p>";
+        echo $code;
+    }
+//  function checkErabiltzailea(erabiltzaile, pasahitza){TODO: BETE}
+?>
