2ad66ccb27905c38fe6181a5d3c9c64a143aeb85
Mikel Iceta Tena <rascajones@gmail.com>
2018-01-07 23:33:46 +0100

    Updated berria_sortu.php
    Ahora hace cosas como subir el articulo realmente

--- www/berria_sortu.php
+++ www/berria_sortu.php
@@ -1,1 +1,10 @@
-aca ayuda llenenme
+<!-- Berri bat igotzeko orri zatia -->
+<?php
+include 'db.php';
+echo $_POST['euskaraz'];
+igoArtikulua($_POST['title'], $_POST['author'], $_POST['content'], $_POST['euskaraz']);
+
+echo "Artikulua igo da, itxaron";
+header("refresh:2;url=main.php");
+die();
+?>
